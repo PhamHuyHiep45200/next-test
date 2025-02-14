53201054e3b6209cf99b187ffd00474c08a16dec
Huy Hiep <hiepph3@rikkeisoft.com>
2025-02-14 16:52:05 +0700

    rết

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-'use client'
+"use client";
 import ListCard from "@/components/ListCard";
 import FacebookLogin from "@greatsumini/react-facebook-login";
 import Link from "next/link";
 // import FacebookLogin from 'react-facebook-login';
 import { Suspense, useEffect } from "react";
+
+declare global {
+  interface Window {
+    fbAsyncInit: () => void;
+  }
+}
+
+declare const FB: any;
 
 const getData = async (params: any) => {
   const res = await fetch(`http://localhost:5000/data?page=${params.page}`, {
@@ -21,7 +29,6 @@ export default function Home(router: any) {
   const { params, searchParams } = router;
   console.log(params, searchParams);
 
-
   // const data = await getData({ page: searchParams.page || 1 });
 
   const query = searchParams?.query || "";
@@ -30,36 +37,41 @@ export default function Home(router: any) {
   const loadSDK = () => {
     window.fbAsyncInit = function () {
       FB.init({
-        appId: '970633574589757',
+        appId: "970633574589757",
         cookie: true,
         xfbml: true,
-        version: 'v22.0'
+        version: "v22.0",
       });
 
       FB.AppEvents.logPageView();
-
     };
 
     (function (d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) { return; }
-      js = d.createElement(s); js.id = id;
+      var js: HTMLScriptElement,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s) as HTMLScriptElement;
+      js.id = id;
       js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-  }
+      if (fjs.parentNode) {
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    })(document, "script", "facebook-jssdk");
+  };
 
   const getFacebookLoginUrl = () => {
     FB.login(function (response) {
       console.log(response);
     });
-  }
+  };
 
   useEffect(() => {
     if (window) {
-      loadSDK()
+      loadSDK();
     }
-  }, [])
+  }, []);
   return (
     <main className="flex min-h-screen flex-col p-24">
       <div className="flex justify-end">
@@ -73,17 +85,17 @@ export default function Home(router: any) {
         <FacebookLogin
           appId="1269005241057356"
           onSuccess={(response) => {
-            console.log('Login Success!', response);
+            console.log("Login Success!", response);
           }}
           onFail={(error) => {
-            console.log('Login Failed!', error);
+            console.log("Login Failed!", error);
           }}
           onProfileSuccess={(response) => {
-            console.log('Get Profile Success!', response);
+            console.log("Get Profile Success!", response);
           }}
           scope="email,public_profile"
           initParams={{
-            version: 'v16.0',
+            version: "v16.0",
             xfbml: true,
           }}
         />
