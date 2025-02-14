29d57dcd11a8f97c6466972eb1ca2b0f00a2956a
Huy Hiep <hiepph3@rikkeisoft.com>
2025-02-14 15:50:06 +0700

    rết

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import ListCard from "@/components/ListCard";
 import FacebookLogin from "@greatsumini/react-facebook-login";
 import Link from "next/link";
@@ -21,11 +21,39 @@ export default async function Home(router: any) {
   const { params, searchParams } = router;
   console.log(params, searchParams);
 
-
   // const data = await getData({ page: searchParams.page || 1 });
 
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
+
+  const loginFB = () => {
+    console.log('zo');
+
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: "970633574589757",
+        cookie: true,
+        xfbml: true,
+        version: "v22.0",
+      });
+    };
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+    window.FB.login(function (response) {
+      if (response.status === "connected") {
+        console.log("FB token: " + response.authResponse.accessToken);
+      }
+    });
+  };
   return (
     <main className="flex min-h-screen flex-col p-24">
       <div className="flex justify-end">
@@ -35,7 +63,7 @@ export default async function Home(router: any) {
         key={query + currentPage}
         fallback={<div>loading..................</div>}
       >
-        <FacebookLogin
+        {/* <FacebookLogin
           appId="970633574589757"
           onSuccess={(response) => {
             console.log('Login Success!', response);
@@ -51,7 +79,8 @@ export default async function Home(router: any) {
             // version: 'v22.0',
             xfbml: true,
           }}
-        />
+        /> */}
+        <button onClick={loginFB}>login fb</button>
         {/* <FacebookLogin
           buttonStyle={{ padding: "6px" }}
           appId="970633574589757"  // we need to get this from facebook developer console by setting the app.
